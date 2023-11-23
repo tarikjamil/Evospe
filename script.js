3373dc18706d106eaa6522ce5fd534c446223d31
tarikjamil <tarikjamill@gmail.com>
2023-11-23 13:13:46 +0100

    initial

---
+++ script.js
@@ -0,0 +1,44 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// On Page Load
+function pageLoad() {
+  let tl = gsap.timeline();
+  tl.to(".main-wrapper", {
+    opacity: 1,
+    ease: "Quint.easeOut",
+    duration: 0.5,
+  });
+  tl.from("[animation=loading-img]", {
+    scale: 1.2,
+    opacity: "0",
+    stagger: { each: 0.1, from: "start" },
+    ease: "Quint.easeOut",
+    duration: 1,
+  });
+  tl.from("[animation=loading]", {
+    y: "100%",
+    opacity: "0",
+    stagger: { each: 0.1, from: "start" },
+    ease: "Quint.easeOut",
+    duration: 1,
+  });
+}
+pageLoad();
+
+var inputFields = document.querySelectorAll(".text-field, .text-area");
+var fieldLabels = document.querySelectorAll(".field-label");
+
+function addEventListeners(elements, eventType) {
+  elements.forEach(function (element, index) {
+    element.addEventListener(eventType, function () {
+      fieldLabels[index].classList.add("focused");
+    });
+
+    element.addEventListener("blur", function () {
+      fieldLabels[index].classList.remove("focused");
+    });
+  });
+}
+
+addEventListeners(inputFields, "focus");
+addEventListeners(inputFields, "blur");
